c93a56d170f954a14e84249ba331e4fd6d634575
torresnr <torresnr@clarkson.edu>
2022-09-30 09:01:49 -0400

    MessageClackData 09/30/22

--- src/data/MessageClackData.java
+++ src/data/MessageClackData.java
@@ -2,19 +2,57 @@ package data;
 
 import java.util.Date;
 
+/* Subclass to ClackData
+* implements an instant message function
+* the 'message'
+ */
+
 public class MessageClackData extends ClackData {
-    private:
-        String message; // string representing instant message
-    public:
-        // constructor to set up username, message, and type using super constructor
-        MessageClackData(String userName, string message, int type) {
+    private String message; /* string representing instant message */
+
+    /* constructor to set up username, message, and type using super constructor
+     */
+    public MessageClackData(String userName, String message, int type) {
         super(userName, type);
         this.message = message;
-        }
+    }
 
-        // default constructor calling another constructor
-        MessageClackData() {
-        this("Unknown User", null, CONSTANT_DEFAULT_TYPE)
-        }
+        /* default constructor calling another constructor
+         */
+    public MessageClackData() {
+        this("Anon", null, -1);
+    }
 
+    /* return instant message
+     */
+    public String getData() {
+        return message;
+    }
+
+    /* hash code
+    * overridden
+     */
+    public int hashCode() {
+        return message.hashCode() * userName.hashCode() * type;
+    }
+
+    /* equals function
+    * overridden
+     */
+    public boolean equals(Object other) {
+        MessageClackData otherMessage = (MessageClackData)other;
+        return this.userName == otherMessage.userName &&
+                this.type == otherMessage.type &&
+                this.message == otherMessage.message;
+    }
+
+    /* toString override
+    * returns a full description of the class with all instance
+    * variables, including those in the superclass
+     */
+    public String toString() {
+        return "The username is: " + getUserName() + "\n" +
+                "The type is: " + getType() + "\n" +
+                "The message is: " + message + "\n";
+    }
 }
