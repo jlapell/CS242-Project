8bcb7e0656f32eda5678eef2ab5167e94c16e69e
torresnr <torresnr@clarkson.edu>
2022-10-01 17:27:45 -0400

    Edited TestClackServer 10/01/22

--- src/test/TestClackServer.java
+++ src/test/TestClackServer.java
@@ -1,4 +1,22 @@
 package test;
 
+import main.ClackServer;
+
+/**
+ * test driver for ClackServer
+ * did not add tests for functions that are not yet implemented
+ */
 public class TestClackServer {
+    public static void main(String[] args) {
+        ClackServer testDefault = new ClackServer();
+        System.out.println(testDefault.getPort());
+        System.out.println(testDefault.hashCode());
+        System.out.println(testDefault.toString());
+
+        ClackServer testPort = new ClackServer(1000);
+        System.out.println(testPort.getPort());
+        System.out.println(testPort.hashCode());
+        System.out.println(testPort.equals(testDefault));
+        System.out.println(testPort.toString());
+    }
 }
